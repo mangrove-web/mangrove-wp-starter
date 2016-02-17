9de168dd82b43aef566128cd94c9180b3a40d331
Jonathan Denni <jonsdenni@gmail.com>
2016-02-17 22:20:41 +0100

    add bootstrap.js and flexslider.js CDN

--- wp-content/themes/_mangrove-starter/functions.php
+++ wp-content/themes/_mangrove-starter/functions.php
@@ -9,6 +9,8 @@ add_action( 'wp_enqueue_scripts', 'Mangrove\enqueue_styles'   );
 
 /* Functions */
 function enqueue_scripts(){
+	/* Note: Add dependencies to the dependency array instead of enqueueing separately */
+	// wp_enqueue_script('bootstrap'); // uncomment this if bootstrap.js is needed directly by markup
 	// wp_enqueue_script('mangrove-custom');
 }
 function enqueue_styles(){
@@ -16,11 +18,17 @@ function enqueue_styles(){
 	wp_enqueue_style( 'mg_custom' );
 }
 function register_scripts(){
+	/* Note: Add dependencies to the dependency array instead of enqueueing separately */
+	wp_register_script( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' );
+	wp_register_script( 'flexslider', '//cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.0/jquery.flexslider.min.js' );
+
 	$js_dir = get_template_directory_uri() . '/library/js/min';
 	wp_register_script(
-		'mangrove-custom',
-		$js_dir . '/mangrove-custom.js',
-		array('jquery')
+		$handle       = 'mangrove-custom',
+		$source       = $js_dir . '/mangrove-custom.js',
+		$dependencies = array( 'jquery' ),
+		$version      = 1,
+		$in_footer    = true
 	);
 }
 function register_styles(){
