53ee9a3d6c2e1a00993992ce28f3b9282b63affb
Jonathan Denni <jonsdenni@gmail.com>
2016-02-17 21:20:52 +0100

    remove unused admin customization

--- wp-content/themes/_mangrove-starter/functions-bones.php
+++ wp-content/themes/_mangrove-starter/functions-bones.php
@@ -14,15 +14,6 @@ require_once('library/bones.php');						// core functions (don't remove)
 // Shortcodes
 require_once('library/shortcodes.php');
 
-// Admin Functions (commented out by default)
-// require_once('library/admin.php');				 // custom admin functions
-
-// Custom Backend Footer
-add_filter('admin_footer_text', 'wp_bootstrap_custom_admin_footer');
-function wp_bootstrap_custom_admin_footer() {
-	echo '<span id="footer-thankyou">Developed by <a href="http://320press.com" target="_blank">320press</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.';
-}
-
 // adding it to the admin area
 add_filter('admin_footer_text', 'wp_bootstrap_custom_admin_footer');
 
