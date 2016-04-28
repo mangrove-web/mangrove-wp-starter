4099d665a97884d2af3f711e2a74d6372e4a38ae
Maiya Holliday <maiya@mangrove-web.com>
2016-04-27 20:33:14 -0700

    remove old theme

--- wp-content/themes/_mangrove-wp-bootstrap-master-3.0/library/js/mangrove-custom.js
+++
@@ -1,8 +0,0 @@
-jQuery(function() {
-  jQuery('.carousel').find('.item:first').addClass('active');
-  jQuery('.carousel-indicators').find('.item:first').addClass('active');
-  jQuery('.carousel').carousel({
-    interval: false
-    });
-});
-
